99ea0f5b656718430c4e8472e7b858623106b09b
syarif2002 <rongodsyarif@gmail.com>
2022-12-19 13:25:40 +0700

    add pegawai page

--- app.js
+++ app.js
@@ -6,26 +6,25 @@ const Rak = require('./models/Rak');
 const Jenis = require('./models/Jenis');
 const Satuan = require('./models/Satuan');
 const Barang = require('./models/Barang');
-
+const Pegawai = require('./models/Pegawai');
+const Users = require('./models/Users');
 
 try {
-    db.authenticate();
-    console.log('Database connected');
-    Satuan.sync()
+  db.authenticate();
+  console.log('Database connected');
+  Pegawai.sync();
 } catch (error) {
-    console.log(error);
+  console.log(error);
 }
 
 app.use(express.json());
-
 
 app.use('/rak', require('./routes/rak'));
 app.use('/jenis', require('./routes/jenis'));
 app.use('/satuan', require('./routes/satuan'));
 app.use('/barang', require('./routes/barang'));
-
-
-
+app.use('/pegawai', require('./routes/pegawai'));
+app.use('/users', require('./routes/users'));
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-})
+  console.log(`Server running on port ${port}`);
+});
