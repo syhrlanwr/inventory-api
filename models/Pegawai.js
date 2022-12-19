1617cd31bf9c2c19a0417d43c5489c488693d5ea
syarif2002 <rongodsyarif@gmail.com>
2022-12-19 13:10:29 +0700

    add pegawai

---
+++ models/Pegawai.js
@@ -0,0 +1,22 @@
+const { Sequelize } = require('sequelize');
+const db = require('../config/Database');
+const { DataTypes } = Sequelize;
+
+const Pegawai = db.define('pegawai', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  nama: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  nip: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+});
+
+module.exports = Pegawai;
