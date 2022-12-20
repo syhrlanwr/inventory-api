4cb173ff27af5e0eceb701af1a7c9bd5f578e371
syhrlanwr <syhrlanwr@gmail.com>
2022-12-20 22:34:12 +0700

    o

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
 const db = require('./config/database');
 const port = 5000;
 const Rak = require('./models/Rak');
 
+const Jenis = require('./models/Jenis');
+const Satuan = require('./models/Satuan');
+const Barang = require('./models/Barang');
+const Pegawai = require('./models/Pegawai');
+const Users = require('./models/Users');
+const cors = require('cors');
 
 try {
   db.authenticate();
   console.log('Database connected');
+  Users.sync();
 } catch (error) {
   console.log(error);
 }
@@ -19,6 +25,11 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/rak', require('./routes/rak'));
+app.use('/jenis', require('./routes/jenis'));
+app.use('/satuan', require('./routes/satuan'));
+app.use('/barang', require('./routes/barang'));
+app.use('/pegawai', require('./routes/pegawai'));
+app.use('/users', require('./routes/users'));
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
--- routes/users.js
+++ routes/users.js
@@ -1,51 +1,46 @@
-const Pegawai = require('../models/Pegawai');
+const Users = require('../models/Users');
 const router = require('express').Router();
 
 router.get('/', async (req, res) => {
-  const pegawai = await Pegawai.findAll();
-  res.json(pegawai);
+    const users = await Users.findAll();
+    res.json(users);
 });
 
 router.post('/', async (req, res) => {
-  const nama = req.body.nama;
-  const username = req.body.username;
-  const password = req.body.password;
-  const users = await users.create({
-    nama: nama,
-    username: username,
-    password: password,
-  });
-  res.json(users);
+    const name = req.body.name;
+    const username = req.body.username;
+    const password = req.body.password;
+    const users = await Users.create({
+        name: name,
+        username: username,
+        password: password
+    });
+    res.json(users);
 });
 
 router.put('/:id', async (req, res) => {
-  const id = req.params.id;
-  const nama = req.body.nama;
-  const username = req.body.username;
-  const password = req.body.password;
-  const users = await users.update(
-    {
-      nama: nama,
-      username: username,
-      password: password,
-    },
-    {
-      where: {
-        id: id,
-      },
-    }
-  );
-  req.json(users);
+    const id = req.params.id;
+    const nama = req.body.nama;
+    const users = await Users.update({
+        nama : nama,
+        username : username,
+        password : password
+    }, {
+        where: {
+            id: id,
+        },
+    });
+    res.json(users);
 });
 
 router.delete('/:id', async (req, res) => {
-  const id = req.params.id;
-  const users = await Users.destroy({
-    where: {
-      id: id,
-    },
-  });
-  res.json(users);
+    const id = req.params.id;
+    const users = await Users.destroy({
+        where: {
+            id: id
+        }
+    });
+    res.json(users);
 });
 
 module.exports = router;
